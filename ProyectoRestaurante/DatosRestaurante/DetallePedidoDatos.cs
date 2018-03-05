dc5c7ca456f142948855d124237487e5616afa3e
Alexander Fernandez Quesada <cocolab13@gmail.com>
2018-03-05 09:30:39 -0600

    Actualizacion de Proyecto Capa Datos

---
+++ ProyectoRestaurante/DatosRestaurante/DetallePedidoDatos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatosRestaurante
+{
+    public class DetallePedidoDatos
+    {
+        public static DataSet SeleccionarTodosporID(int idPedido)
+        {
+            Database db = DatabaseFactory.CreateDatabase("Default");
+
+            SqlCommand comando = new SqlCommand("PA_SeleccionarDetallesPedido");
+            // Es requerido indicar que el tipo es un StoreProcedure
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@idEncabezadoPedido", idPedido);
+            DataSet ds = db.ExecuteReader(comando, "carrera");
+            return ds;
+        }
+    }
+}
